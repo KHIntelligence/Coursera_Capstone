95d27fd7bce167b5d1f997fa14894223694dc10c
KHIntelligence <35332307+KHIntelligence@users.noreply.github.com>
2019-03-18 09:28:51 +0000

    Add files via upload

---
+++ Coursera_Capston.py
@@ -0,0 +1,18 @@
+
+# coding: utf-8
+
+# # Coursera Captsone
+# This will mainly be used for the coursera capstone project.
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course!")
+
